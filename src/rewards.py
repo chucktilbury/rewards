74fbb7933711c4a0fc804c2473d636af75590b9f
Chuck Tilbury <chucktilbury@gmail.com>
2023-08-19 11:25:12 -0500

    temp commit

--- src/rewards.py
+++ src/rewards.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 import sqlite3
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, g, request
+import os
+
+app = Flask(__name__, template_folder="/home/chuck/Src/rewards/src/templates")
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    #return "Hello World!"
+    return render_template('layout.html')
+    #return os.getcwd()
 
 if __name__ == "__main__":
     app.run(debug=True)
